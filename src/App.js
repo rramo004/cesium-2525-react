a587e71f90018d10ea24f6cbca61390a5c8160ae
rramo004 <rramos@Richards-MacBook-Air.local>
2019-03-08 10:24:05 -0800

    Added Cesium to App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Viewer from "cesium/Source/Widgets/Viewer/Viewer";
 
 class App extends Component {
+  
+  componentDidMount() {
+    this.viewer = new Viewer(this.cesiumContainer);
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      return (
+          <div>
+              <div id="cesiumContainer" ref={ element => this.cesiumContainer = element }/>
+          </div>
+      );
   }
 }
 
